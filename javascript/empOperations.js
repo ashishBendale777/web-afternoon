1cb21fbc8ac86851ecebe084063ce362bbbe9b19
technoweit77 <technoweit77@gmail.com>
2025-03-28 10:19:45 +0530

    done

---
+++ javascript/empOperations.js
@@ -0,0 +1,20 @@
+//employee data
+let employee = [
+    {}, {}, {}
+]
+
+//getEmployeeBySalary
+
+let getEmployeeByGreaterSalary = (salary) => {
+
+}
+
+let getEmployeeByLesserSalary = (salary) => {
+
+}
+
+let getEmployeeBetweenSalary = (fromSalary, toSalary) => {
+
+}
+
+export { getEmployeeByGreaterSalary, getEmployeeByGreaterSalary }
